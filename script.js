f5177b40ea4849739bb981d96e4be9d9164999ee
coded-circuit <kabya25ranjan@gmail.com>
2025-06-10 19:04:39 +0530

    Second Commit

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ async function checkWeather(city){
     if(response.status == 404){
         document.querySelector('.error').style.display ='block';
         document.querySelector('.weather').style.display ='none';
+        document.querySelector('.intro').style.display ='none';
     }
     else{
     var data = await response.json();
